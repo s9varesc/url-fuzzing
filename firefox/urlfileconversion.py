3acc514e68d0937479b21a867c5de7238141f5e3
Vera Resch <s9varesc@stud.uni-saarland.de>
2019-12-12 10:21:47 +0100

    split single test file into multiple test files

--- firefox/urlfileconversion.py
+++ firefox/urlfileconversion.py
@@ -13,27 +13,23 @@ parser.add_argument("-dir")
 args = parser.parse_args()
 dir = args.dir
 
-urldata="var gTests = ["
-
-
+f=open("./test_URIs_suffix.txt","r")
+suffix=f.read()
+i=2
 for filename in os.listdir(dir):
+	i+=1
+	urldata="var gTests = ["
 	if filename[0]=="c": #open only component files
 		f=open(dir+"/"+filename, "r")
 		url=f.read()
-		urldata+=url+",\n"
-urldata=urldata[:-2]
-urldata+="];\n"
-
-prefix="\"use strict\";\
-\n var gIoService = Cc[\"@mozilla.org/network/io-service;1\"].getService(Ci.nsIIOService);\n"
-
-f=open("./test_URIs_suffix.txt","r")
-suffix=f.read()
-js_file=prefix+"\n"+urldata+"\n"+suffix
-
-f=open("./test_URIs3.js","w")
-f.write(js_file)
-f.close()
+		urldata+=url+"]\n"
+	prefix="\"use strict\";\
+	\n var gIoService = Cc[\"@mozilla.org/network/io-service;1\"].getService(Ci.nsIIOService);\n"
+	js_file=prefix+"\n"+urldata+"\n"+suffix
+	
+	f=open("generatedTestFiles/test_URIs_"+str(i)+".js","w")
+	f.write(js_file)
+	f.close()
 
 
 	
