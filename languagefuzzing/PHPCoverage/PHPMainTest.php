177316c5bdf020529dc8bbf3eac110c3a52fa611
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-03-30 13:50:40 +0200

    php example coverage

---
+++ languagefuzzing/PHPCoverage/PHPMainTest.php
@@ -0,0 +1,31 @@
+<?php
+require "./vendor/league/uri/src/UriString.php";
+require "./vendor/league/uri-interfaces/src/Contracts/UriException.php";
+require "./vendor/league/uri-interfaces/src/Exceptions/SyntaxError.php";
+
+
+use PHPUnit\Framework\TestCase;
+use League\Uri\UriString;
+use League\Uri\Exceptions\SyntaxError;
+
+class PHPMainTest extends TestCase {
+public function test_urls(){
+$file = fopen("../exampleURLs/plainURLs","r");
+
+while(! feof($file))
+  {
+  $url= fgets($file);
+  //parse_url($url);
+  
+try {
+    UriString::parse($url);
+} catch (Exception $e) {
+   
+}
+  }
+
+fclose($file);
+$this->assertTrue(True);
+}
+}
+?>
