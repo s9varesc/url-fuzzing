ff9db1556f60029be70505115da4eedc06502b82
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-04-13 14:50:52 +0200

    added exception collection in js

--- languagefuzzing/JavaScriptCoverage/TestJavaScriptMain.js
+++ languagefuzzing/JavaScriptCoverage/TestJavaScriptMain.js
@@ -3,6 +3,7 @@
 var URI = require('urijs');
 var fs = require('fs');
 var readline = require('readline');
+var exceptions="";
 
 var rd = readline.createInterface({
     input: fs.createReadStream('../urls/plainURLs'),
@@ -14,8 +15,14 @@ rd.on('line', function(line) {
     try {
         const url=new URI(line);
     }
-    catch(err){}
+    catch(err){
+	exceptions=exceptions+"\n{ url:\""+line+"\",\n exception:\""+err.message+"\"},";
+    }
 });
+
+fs.writeFile('JavaScriptExceptions.txt', exceptionsfinal, (err) => {  
+    if (err) throw err; 
+}) 
 
 
 
