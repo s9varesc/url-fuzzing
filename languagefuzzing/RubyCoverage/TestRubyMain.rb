42aa9251a7ab5720e11b5508c643bbc462994882
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-04-13 15:00:55 +0200

    added exception collection

--- languagefuzzing/RubyCoverage/TestRubyMain.rb
+++ languagefuzzing/RubyCoverage/TestRubyMain.rb
@@ -1,12 +1,15 @@
 #!/usr/bin/ruby
 
 require_relative './testuri/uri'
-
+exceptions=""
 IO.foreach("../urls/plainURLs"){|block| 
 	begin 
 	    uri = URI(block) 
 	    
 	rescue URI::InvalidURIError => e
-	
+	   exceptions+="\n{ url:\""+block+"\",\n exception:\""+e"\"},";
 	end
 }
+    File.open('RubyExceptions.txt', 'w') do |f|  
+      f.write("["+exceptions[0..-2]+"]")  
+    end  
