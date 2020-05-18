2ece86aee18451a1e5dac870402ca7e2e41698dd
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-05-18 17:55:22 +0200

    bug fixes

--- chromium/url_parsing_unittest.cc
+++ chromium/url_parsing_unittest.cc
@@ -78,11 +78,11 @@ TEST(URLParser, Parsing){
 		} 
 		else{
 			if( IsStandard(parse_cases[i].input,scheme)){
-				if (scheme == kFileScheme){
+				if (parse_cases[i].input.compare(0,scheme.end(),kFileScheme)){
 					ParseFileURL(parse_cases[i].input,len, &parsed);
 				}
 				else {
-					if (scheme == kFileSystemScheme){
+					if (parse_cases[i].input.compare(0,scheme.end(),kFileSystemScheme)){
 						ParseFileSystemURL(parse_cases[i].input, len, &parsed);
 					}
 					else{
