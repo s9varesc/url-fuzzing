273b0291a5caf6e84e4d1362235680db8b3f876f
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-06-30 13:48:03 +0200

    list and path issues

--- tribble-additions/testnewversion/tasks.scala
+++ tribble-additions/testnewversion/tasks.scala
@@ -7,6 +7,7 @@ import java.nio.file.Files
 import org.backuity.clist.Command
 import org.log4s.getLogger
 import saarland.cispa.se.tribble.execution.componentExtraction.DictExtractor
+import collection.JavaConverters._
 
 trait Task {
   def execute(): Unit
@@ -26,14 +27,14 @@ final class GenerateTask extends Command("generate", "Generate sample inputs")
     for ((tree, i) <- trees.zipWithIndex) {
       reporter.processTree(i + 1, tree)
       val input = tree.leaves.mkString
-      val outdir=Files.createDirectories(outputDir+"/plain")
+      val outdir=Files.createDirectories(Paths.get(outputDir+"/plain"))
       val path = Files.write(Files.createTempFile(outdir, f"file${i + 1}%06d_${tree.size()}%d_${tree.depth()}%d_", suffix), input.getBytes(StandardCharsets.UTF_8))
       logger.debug(s"Generated $path")
 
       val dictExtractor=new DictExtractor();
       val components=dictExtractor.extract(tree);
       for(comp<-components.asScala){
-        val outdir=Files.createDirectories(outputDir+"/"+comp(0))
+        val outdir=Files.createDirectories(Paths.get(outputDir+"/"+comp(0)))
         val path2 = Files.write(Files.createTempFile(outdir+, f"components_file${i + 1}%06d_${tree.size()}%d_${tree.depth()}%d_", suffix), comp(1).getBytes(StandardCharsets.UTF_8))
         logger.debug(s"Generated $path2")
       }
