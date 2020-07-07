af0a92e10c7c918b47f7a9efa5980f5aaa0e23c2
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-07-07 12:16:51 +0200

    bug fixes

--- languagefuzzing/RubyCoverage/TestRubyMain.rb
+++ languagefuzzing/RubyCoverage/TestRubyMain.rb
@@ -4,7 +4,7 @@ require_relative './testuri/uri'
 exceptions=""
 IO.foreach("../urls/plainURLs"){|block| 
 	begin 
-	    uri = URI(block) 
+	    uri = URI(block[0..-2]) 
 	    
 	rescue URI::InvalidURIError => e
 	   exceptions+="\n{ url:\""+block[0..-2]+"\",\n exception:\""+e.message+"\"},";
