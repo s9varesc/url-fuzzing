d7aa4c908a15f9776df259a21274e49a8f4a4b6f
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-07-08 11:56:13 +0200

    updated firefox file conversion to have around 20 urls per test file

--- firefox/urlfileconversion.py
+++ firefox/urlfileconversion.py
@@ -15,24 +15,51 @@ dir = args.dir
 
 f=open("./test_URIs_suffix.txt","r")
 suffix=f.read()
-i=2
 
-testnames=[]
-for filename in os.listdir(dir):
-	i+=1
-	urldata="var gTests = ["
-	if filename[0]=="c": #open only component files #can be removed, now only component files in folder
-		f=open(dir+"/"+filename, "r")
-		url=f.read()
-		urldata+=url+"];"
-		prefix="\"use strict\";\
+prefix="\"use strict\";\
 	\n var gIoService = Cc[\"@mozilla.org/network/io-service;1\"].getService(Ci.nsIIOService);\n"
-		js_file=prefix+"\n"+urldata+"\n"+suffix
-	
-		f=open("URLTestFiles/test_URIs_"+str(i)+".js","w") #TODO collect multiple inputs in test file
-		testnames+=["test_URIs_"+str(i)+".js"]
-		f.write(js_file)
-		f.close()
+
+#collect url data
+urlcontents=[]
+for filename in os.listdir(dir):
+	f=open(dir+"/"+filename, "r")
+	urlcontents+=[f.read()]
+
+#create test files with ~20 test cases per file
+n=20
+testnames=[]
+testchunks=[urlcontents[i*n:(i+1)*n] for i in range((len(urlcontents)+n-1)//n)]
+testid=2
+for chunk in testchunks:
+	testid+=1
+	urldata="var gTests = ["
+	for url in chunk:
+		urldata+="\n" + url + ","
+	urldata=urldata[:-1]
+	urldata+="];"
+	testname="test_URIs_"+str(testid)+".js"
+	testnames+=[testname]
+	f=open("URLTestFiles/"+testname,"w")
+	f.write(prefix+"\n"+urldata+"\n"+suffix)
+	f.close()
+
+#i=2
+#testnames=[]
+#for filename in os.listdir(dir):
+#	i+=1
+#	urldata="var gTests = ["
+#	if filename[0]=="c": #open only component files #can be removed, now only component files in folder
+#		f=open(dir+"/"+filename, "r")
+#		url=f.read()
+#		urldata+=url+"];"
+#		prefix="\"use strict\";\
+#	\n var gIoService = Cc[\"@mozilla.org/network/io-service;1\"].getService(Ci.nsIIOService);\n"
+#		js_file=prefix+"\n"+urldata+"\n"+suffix
+#	
+#		f=open("URLTestFiles/test_URIs_"+str(i)+".js","w") #TODO collect multiple inputs in test file
+#		testnames+=["test_URIs_"+str(i)+".js"]
+#		f.write(js_file)
+#		f.close()
 	
 xpcshellinicontent="[DEFAULT]\nhead = head_channels.js head_cache.js head_cache2.js head_cookies.js\nretry = False\n"
 for test in testnames:
