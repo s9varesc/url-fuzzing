a65826396b19d32f010d789be9f77c7657cecf53
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-07-23 11:50:22 +0200

    removed unicode rules

--- livingstandard-url.scala
+++ livingstandard-url.scala
@@ -48,7 +48,7 @@ Grammar(
 		| ("655" ~ ("[0-2]".regex) ~ 'digit)
 		| ("6553" ~ ("[0-5]".regex)),
   'URLunit := 'URLcodePoint | 'percentEncodedByte,
-  'URLcodePoint := 'reserved | 'unreserved | 'unicode,
+  'URLcodePoint := 'reserved | 'unreserved, //| 'unicode,
   'reserved := ":" | "/" | "?" | "#" | "[" | "]" | "@" | 'subdelims,
   'subdelims := "!" | "$" | "&" | "'" | "(" | ")" | "*" | "+" | "," | ";" | "=",
   'unreserved := 'alphanum | "-" | "." | "_" | "~",
@@ -63,25 +63,22 @@ Grammar(
   //u+F000-u+FCFF, u+FE00-u+FFFF
   //u+FD00 - u+FDCF
   //u+FDF0-u+FDFF
-  'unicode := "U+" ~ (("0" ~ (("0" ~ "A" ~ 'unicodeHEX) | (("[1-9]".regex | "[A-F]".regex) ~ 'unicodeHEX.rep(2, 2))))
-    | (("[1-9]".regex | "A" | "B" | "C" | "E") ~ 'unicodeHEX.rep(3, 3))
-    | ("10" ~ ((('digit | "[A-E]".regex) ~ 'unicodeHEX.rep(3, 3))
-    | ("F" ~ ('digit | "[A-E]".regex) ~ 'unicodeHEX.rep(2, 2))
-    | ("FF" ~ ('digit | "[A-E]".regex) ~ 'unicodeHEX)
-    | ("FFF" ~ ('digit | "[A-D]".regex))))
-    | ("D" ~ "[0-7]".regex ~ 'unicodeHEX.rep(2, 2))
-    | ("F" ~ ('digit | "A" | "B" | "C" | "E" | "F") ~ 'unicodeHEX.rep(2, 2))
-    | ("FD" ~ ('digit | "A" | "B" | "C") ~ 'unicodeHEX)
-    | ("FDF" ~ 'unicodeHEX)),
+  //'unicode := "U+" ~ (("0" ~ (("0" ~ "A" ~ 'unicodeHEX) | (("[1-9]".regex | "[A-F]".regex) ~ 'unicodeHEX.rep(2, 2))))
+  //  | (("[1-9]".regex | "A" | "B" | "C" | "E") ~ 'unicodeHEX.rep(3, 3))
+  //  | ("10" ~ ((('digit | "[A-E]".regex) ~ 'unicodeHEX.rep(3, 3))
+  //  | ("F" ~ ('digit | "[A-E]".regex) ~ 'unicodeHEX.rep(2, 2))
+  //  | ("FF" ~ ('digit | "[A-E]".regex) ~ 'unicodeHEX)
+  //  | ("FFF" ~ ('digit | "[A-D]".regex))))
+  //  | ("D" ~ "[0-7]".regex ~ 'unicodeHEX.rep(2, 2))
+  //  | ("F" ~ ('digit | "A" | "B" | "C" | "E" | "F") ~ 'unicodeHEX.rep(2, 2))
+  //  | ("FD" ~ ('digit | "A" | "B" | "C") ~ 'unicodeHEX)
+  //  | ("FDF" ~ 'unicodeHEX)),
   'host := ('userinfo ~ "@").? ~ 'domain,
   //missing ipfuture, zoneid
   'domain := ('unreserved | 'percentEncodedByte | 'subdelims ).rep  | 'ipv4address | ("[" ~ 'ipv6address ~ "]"), //TODO maybe remove subdelims
   'userinfo := ('unreserved | 'percentEncodedByte | 'subdelims | ":").rep,
   'ipv4address := 'decoctet ~ "." ~ 'decoctet ~ "." ~ 'decoctet ~ "." ~ 'decoctet,
   'ipv6address := (('h16 ~ ":").rep(6, 6) ~ 'ls32)
-    //| ("::" ~ ('h16 ~ ":").rep(5, 5) ~ 'ls32) //TODO dont use :: to shorten single 0 field, use for longest and first consecutive 0 fields
-    //| ('h16 ~ "::" ~ ('h16 ~ ":").rep(4, 4) ~ 'ls32)
-    //| ((('h16 ~ ":").rep(0, 1) ).? ~ "::" ~ ('h16 ~ ":").rep(3, 3) ~ 'ls32)
     | ((('h16 ~ ":").rep(0, 1) ~ 'h16).? ~ "::" ~ ('h16 ~ ":").rep(2, 2) ~ 'ls32)
     | ((('h16 ~ ":").rep(0, 2) ~ 'h16).? ~ "::" ~ 'h16 ~ ":" ~ 'ls32)
     | ((('h16 ~ ":").rep(0, 3) ~ 'h16).? ~ "::" ~ 'ls32)
@@ -97,7 +94,7 @@ Grammar(
   'alphanum := "[a-zA-Z0-9]".regex,
   'alpha := "[a-zA-Z]".regex,
   'hexdig := ("[a-f]".regex) | 'digit,
-  'unicodeHEX := 'digit | ("[A-F]".regex),
+  //'unicodeHEX := 'digit | ("[A-F]".regex),
   'percentEncodedByte := "%" ~ 'hexdig ~ 'hexdig,
   'ws := " " | "\\t" | "\\r" | "\\n" , //TODO make sure the final tests contain \n etc
   'empty := ""
