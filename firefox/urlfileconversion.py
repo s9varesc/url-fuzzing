e988700872633c0be132c12d199d23f590218b07
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-11-10 15:49:04 +0100

    nr of testfiles in ff back to original

--- firefox/urlfileconversion.py
+++ firefox/urlfileconversion.py
@@ -26,7 +26,7 @@ for filename in os.listdir(dir):
 	urlcontents+=[f.read()]
 
 #create test files with ~n test cases per file
-n=30
+n=1
 testnames=[]
 testchunks=[urlcontents[i*n:(i+1)*n] for i in range((len(urlcontents)+n-1)//n)]
 testid=2
