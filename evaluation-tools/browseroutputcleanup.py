65926c75e412849eac7bead98991e73fc331cb9c
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-11-12 13:30:40 +0100

    added python script to extrct info from browser output

---
+++ evaluation-tools/browseroutputcleanup.py
@@ -0,0 +1,40 @@
+import argparse
+import os
+import subprocess
+import json
+
+
+# splits the output log created by a Browser into [Browser]Exceptions.txt and [Browser]Errors.txt for further evaluation
+
+# open the given directory and read the available log files
+parser = argparse.ArgumentParser()
+parser.add_argument("-dir")
+args = parser.parse_args()
+dir = args.dir
+
+for file in os.listdir(dir):
+	if file.endswith('.log'):
+		with open(dir +"/"+ file) as f:
+			logfile=f.read()  
+			browsername=file[:-10] remove "output.log"
+
+		exceptionfiledata="\n" #match other exception files
+		errorfiledata=""
+		splitlogfile=logfile.split("\n")
+		for line in splitlogfile:
+			if "{url" in line:
+				if "error:{component:" in line:
+					#failed component match
+					errorfiledata+=line+"\n"
+				else:
+					#exception during parsing
+					exceptionfiledata+=line+",\n" #matches format of languages
+	
+	#write resulting files
+	exfile=open(browsername+"Exceptions.txt", "w")
+	exfile.write(exceptionfiledata)
+	exfile.close()
+
+	erfile=open(browsername+"Errors.txt", "w")
+	erfile.write(errorfiledata)
+	erfile.close()	
