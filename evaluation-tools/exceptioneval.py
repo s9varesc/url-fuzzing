23a127d7ae9c648fd3f17022e89b847a6e9fec73
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-11-12 11:56:39 +0100

    updated exception evaluation

--- evaluation-tools/exceptioneval.py
+++ evaluation-tools/exceptioneval.py
@@ -3,6 +3,40 @@ import os
 import subprocess
 import json
 
+# correct escaping 
+def fixdatapoint(datapoint):
+	print(datapoint)
+	if len(datapoint)<=2: return datapoint
+	expectedquotationmarks=8
+	res=datapoint
+	#if datapoint.count("\"") > expectedquotationmarks:
+	[dp1, dp2]=datapoint.split("\n")
+	pre1=dp1[:9] #{ "url":"
+	p1=dp1[9:-2] #url contents
+	post1=dp1[-2:] # ",
+
+	pre2=dp2[:14]#"exception":"
+	p2=dp2[14:-2] #exception contents
+	post2=dp2[-2:] # "}
+
+	p1=p1.replace("\\", "\\\\")
+	p2=p2.replace("\\", "\\\\")
+
+	p1=p1.replace("\"", "\\\"")
+	p2=p2.replace("\"", "\\\"")
+		
+	if p1[-1:]=="\\":
+		p1=p1+" "
+
+	if p2[-1:]=="\\":
+		p2=p2+" "
+
+		
+		
+	res=pre1+p1+post1+pre2+p2+post2
+	return res
+
+
 #
 # evaluates the collected exceptions
 #
@@ -15,8 +49,10 @@ args = parser.parse_args()
 dir = args.dir
 
 for file in os.listdir(dir):
-	if file.endswith('URLs.txt'):   
-		plainURLs=file.read()  
+	if file.endswith('URLs'):
+		with open(dir +"/"+ file) as f:
+			plainURLs=f.read()   
+		
 
 # all urls that were tested
 
@@ -25,20 +61,27 @@ urls=plainURLs.split("\n")
 parsers={}
 # read the exceptions into dictionaries
 for exfile in os.listdir(dir):
-	if exfile.endswith('Exceptions.txt'): 
-		parsername=exfile[:-len("Exceptions.txt")]
-		with open(exfile) as f:
+	if 'Exceptions' in exfile: 
+		parsername=exfile.replace("Exceptions","").replace(".txt","")
+		with open(dir +"/"+ exfile) as f:
 			data=f.read()
-		#TODO
-		data.replace("{ url:", "{ \"url\":") #check if replacing really necessary
-		data.replace("exception:", "\"exception\":")
+		
+		data = data.replace("{ url:", "{ \"url\":") 
+		data = data.replace("exception:", "\"exception\":")
+		
+		data = data[1:-1]
+		
 
-		sep="},"
-		splitdata=[d+sep for d in data.split(data) if d] #one entry per {url:...,exception:...},
-
+		sep="},\n" 
+		splitdata=[d+"}" for d in data.split(sep) if d] #one entry per {url:...,exception:...},
+		#print(splitdata[2])
 		#produce dictionary with {url1 : exception1, url2:...}
 		datadict={}
+		print(parsername)
 		for datapoint in splitdata:
+			#datapoint=datapoint[1:]
+			#print(datapoint)
+			datapoint=fixdatapoint(datapoint)
 			tempdict=json.loads(datapoint)
 			datadict[tempdict["url"]]=tempdict["exception"]
 
@@ -62,7 +105,7 @@ for parser in parsers:
 	errtypes={}
 	for errdata in parsers[parser]:
 		temp=parsers[parser][errdata]
-		temp.replace(errdata, "")
+		temp = temp.replace(errdata, "")
 		if temp in errtypes: 
 			errtypes[temp]+=[errdata]
 		else:
@@ -71,21 +114,28 @@ for parser in parsers:
 	# place results
 	countresults["nrerrtypes"]=len(errtypes)
 	countresults["errtypes"]=errtypes
-	parserranking[parsername]=countresults
+	parserranking[parser]=countresults
 
 #url ranking
 for url in urls:
-	urlranking[url]=[]
+	#urlranking[url]=[]
 	for parser in parsers:
 		if url in parsers[parser]:
+			#print(url)
+			if url not in urlranking:
+				urlranking[url]=[]
 			urlranking[url]+=[parser]
 
 print("Results: \n")
-print("number of urls tested: "+ len(urls))
-print("number of parsers: "+len(parsers))
+print("number of urls tested: "+ str(len(urls)))
+print("number of parsers: "+str(len(parsers)))
 print("ranking of parsers (unsorted)")
 print(parserranking)
 print("ranking of urls (unsorted)")
+print(urlranking)
+
+
+
 
 
 
