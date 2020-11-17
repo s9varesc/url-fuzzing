c832b654bf99ced94ebdf3a68e40b8f6ae270d82
Vera Resch <s9varesc@stud.uni-saarland.de>
2020-11-17 11:54:26 +0100

    check python tests

--- languagefuzzing/PythonCoverage/TestPythonMain.py
+++ languagefuzzing/PythonCoverage/TestPythonMain.py
@@ -13,7 +13,6 @@ for line in lines:
 for url in urls:
     try:
        urlparse(url)
-       print(url)
     except Exception as e:
         exceptions+="\n{\"url\":\""+url+"\", \"exception\":\""+str(e)+"\"}"
 
