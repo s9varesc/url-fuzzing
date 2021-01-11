f0f60b78140c26d4fcd5bdfcda462f04ffe64cf0
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-01-11 12:37:47 +0100

    style fixing tools for chromium reports

---
+++ chromium/stylefix.py
@@ -0,0 +1,19 @@
+import argparse
+import os
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-dir")
+args = parser.parse_args()
+dir = args.dir
+
+for filename in os.listdir(dir):
+    #print filename
+    f=open(dir+"/"+filename)
+    s = f.read()
+    if filename.endsWith(".html"):
+        s = s.replace('../../../../../../../style.css', './style.css')
+    #print "replaced in " + filename
+    f=open(dir+"/"+filename, "w")
+    f.write(s)
+    f.close()
