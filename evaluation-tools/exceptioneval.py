c42d2eb50d8121be94a7c9240e6ca4a61c23bd94
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-01-25 13:48:07 +0100

    improved exceptioneval.py

--- evaluation-tools/exceptioneval.py
+++ evaluation-tools/exceptioneval.py
@@ -7,34 +7,21 @@ import json
 def escaping(data):
 	tmp=data.replace("\\", "\\\\")
 	tmp=tmp.replace("\"", "\\\"" )
-	tmp=tmp.replace("\'", "\\\'")
 	return tmp
-def fixdatapoint(datapoint): #TODO use escapebetween
-	
+
+def fixdatapoint(datapoint): 
+	# fixes escaping in exception data
 	if len(datapoint)<=2: return datapoint
+	
+	# {"url":"
+	# ", "exception":"
+	# "}
+	s1="{\"url\":\""
+	s2="\", \"exception\":\""
+	s3="\"}"
 	res=datapoint
-	
-	[dp1, dp2]=datapoint.split("\", \"exception\":\"")
-	dp1=dp1+"\","
-	
-	pre1=dp1[:8] #{ "url":"
-	p1=dp1[8:-2] #url contents
-	post1=dp1[-2:] + " \"exception\":\""
-
-	pre2=""# 
-	p2=dp2[:-2] #exception contents
-	post2=dp2[-2:] # "}
-
-	p1=escaping(p1)
-	p2=escaping(p2)
-
-	if p1[-1:]=="\\":
-		p1=p1+" "
-
-	if p2[-1:]=="\\":
-		p2=p2+" "
-
-	res=pre1+p1+post1+pre2+p2+post2
+	res=escapebetween(s1, s2, res)
+	res=escapebetween(s2, s3, res)
 	return res
 
 def fixerrordatapoint(datapoint):
@@ -64,11 +51,11 @@ def fixerrordatapoint(datapoint):
 def escapebetween(s1, s2, data):
 	wd=data
 	i1=wd.find(s1)
-	i2=wd.find(s2)
+	i2=wd.rfind(s2)
 
-	content=wd[i1+len(s1):s2]
+	content=wd[i1+len(s1):i2]
 	escapedcontent=escaping(content)
-	return wd[:i1+len(s1)]+escapedcontent+wd[s2:]
+	return wd[:i1+len(s1)]+escapedcontent+wd[i2:]
 
 #
 # evaluates the collected exceptions and errors
@@ -80,6 +67,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-dir")
 args = parser.parse_args()
 dir = args.dir
+if dir[-1:]!="/":dir+="/"
 
 
 for file in os.listdir(dir):
@@ -108,15 +96,18 @@ for exfile in os.listdir(dir):
 
 		sep="}\n" 
 		splitdata=[d+"}" for d in data.split(sep) if d] #one entry per {url:...,exception:...}
-		#print(splitdata[2])
+
 		#produce dictionary with {url1 : exception1, url2:...}
 		datadict={}
 		
 		for datapoint in splitdata:
 			#datapoint=datapoint[1:]
-			#print(datapoint)
+			
+			if datapoint[-2:]=="}}":datapoint=datapoint[:-1]
 			datapoint=fixdatapoint(datapoint)
+			
 			tempdict=json.loads(datapoint)
+			
 			datadict[tempdict["url"]]=tempdict["exception"]
 
 		# {parsername1 : {url1:exception1, url2:...},
@@ -162,15 +153,6 @@ for url in urls:
 			urlranking[url]+=[parser]
 
 
-pfile=open( dir+"../evaldata/"+"parsercomp.json", "w") 
-pfile.write(parserranking)
-pfile.close()
-
-ufile=open( dir+"../evaldata/"+"urlcomp.json", "w")
-ufile.write(urlranking)
-ufile.close()
-
-
 
 
 # create error dictionaries:
@@ -179,22 +161,41 @@ ufile.close()
 
 errorranking={}
 for file in os.listdir(dir):
-	if file.contains('Errors'):
+	if 'Errors' in file:
 		with open(dir +"/"+ file) as f:
 			errdata=f.read()   
 			name=file.replace("Errors.txt", "")
-			esplit=errdata.split("\n")
-			fixederrdata={}
+			rawesplit=errdata.split("\n")
+			esplit=[]
+			esplit=[x for x in esplit if x]
+			errorranking[name]=[]
 			for errd in esplit:
-				fixederrdata+=[fixerrordatapoint(errd)]
-			tmp=json.loads(fixederrdata)
-			errorranking[name]=tmp #TODO make sure its a list
+				
+				try:
+					tmp=json.loads(fixerrordatapoint(errd))
+					errorranking[name]+=[tmp] #TODO make sure its a list
+				except json.decoder.JSONDecodeError as e:
+					print(e)
+					print(fixerrordatapoint(errd))
 
 
+
+#write results to file, files will be used by produceResultPresentation.py to create a nice html overview
+
+if not os.path.exists(dir+"../evaldata"):
+	os.mkdir(dir+"../evaldata")
+
+pfile=open( dir+"../evaldata/"+"parsercomp.json", "w") 
+pfile.write(json.dumps(parserranking))
+pfile.close()
+
+ufile=open( dir+"../evaldata/"+"urlcomp.json", "w")
+ufile.write(json.dumps(urlranking))
+ufile.close()
 
 
 efile=open( dir+"../evaldata/"+"errorcomp.json", "w")
-efile.write(errorranking)
+efile.write(json.dumps(errorranking))
 efile.close()
 
 
