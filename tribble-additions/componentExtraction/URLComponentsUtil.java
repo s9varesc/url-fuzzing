889468302e6541dfb3a6edfb8b99f8ffbf88f6a1
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-02-12 13:58:02 +0100

    bug fix

--- tribble-additions/componentExtraction/URLComponentsUtil.java
+++ tribble-additions/componentExtraction/URLComponentsUtil.java
@@ -136,7 +136,7 @@ public class URLComponentsUtil {
 		int index=0;
 		while(index<input.length() && index >= 0){
 			int next=input.indexOf(slash, index+1); //exclude leading / from search
-			result.add(input.substring(index, (next>=0 ? next : input.length()-1))); //exclude trailing / from segment
+			result.add(input.substring(index, (next!=-1 ? next : input.length()-1))); //exclude trailing / from segment
 			index=next;
 		}
 
