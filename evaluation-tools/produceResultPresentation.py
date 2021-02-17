0814574cfd046558d6433693e0db4d5a10d710e0
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-02-17 17:36:13 +0100

    working on backtick escaping

--- evaluation-tools/produceResultPresentation.py
+++ evaluation-tools/produceResultPresentation.py
@@ -49,7 +49,7 @@ def url_escape_md(data):
 	escbt="`"
 	for i in range(1, maxnrbt):
 		escbt+="`"
-	res=res.replace("|", "&#124;")
+	#res=res.replace("|", "&#124;")
 	return escbt+" "+res+" "+escbt
 
 
