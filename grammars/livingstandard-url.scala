6f1d370fe59e5bc96f2084e1d4f1f5bc63307603
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-02-19 14:23:34 +0100

    changes to international host

--- grammars/livingstandard-url.scala
+++ grammars/livingstandard-url.scala
@@ -52,7 +52,7 @@ Grammar(
   
   //'host := ('userinfo ~ "@").? ~ 'domain,  //TODO userinfo is deprecated
   'domain := 'internationalHost | 'hostAllowed.rep(1), 
-  'internationalHost := ("xn--").? ~ ('alphanum | "-").rep(1), 
+  'internationalHost := ("xn--").? ~ ('alphanum ~ ("-" | 'alphanum)).rep(1),///('alphanum | "-").rep(1), 
   //'userinfo := 'userinfoCodePoint ~ 'userinfoCodePoint.rep ~ (":" ~ 'userinfoCodePoint ~ 'userinfoCodePoint.rep).?, 
   'ipv4address := 'decoctet ~ "." ~ 'decoctet ~ "." ~ 'decoctet ~ "." ~ 'decoctet,
   'ipv6address := (('h16 ~ ":").rep(6, 6) ~ 'ls32)
