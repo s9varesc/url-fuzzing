ba3a302670524d11f5a0d136f3290c57e3ec71d3
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-03-02 14:48:09 +0100

    query fix

--- tribble-additions/componentExtraction/UniversalURLComponentsBuilder.java
+++ tribble-additions/componentExtraction/UniversalURLComponentsBuilder.java
@@ -375,7 +375,7 @@ public class UniversalURLComponentsBuilder extends UniversalComponentsBuilder {
             }
             components.put("path", path);
             String rpath=components.get("relative_path");
-            if(relative.startsWith("?")|| relative.startsWith("/")){ // relative contains path or host or  query (and fragment)
+            if(relative.startsWith("?")|| rpath!=null || relative.startsWith("/")){ // relative contains path or host or  query (and fragment)
                 components.put("query", components.get("relative_query"));
                 components.put("fragment", components.get("relative_fragment"));
             }
