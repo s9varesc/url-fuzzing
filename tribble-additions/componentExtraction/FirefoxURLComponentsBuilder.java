5c12ccf117aa7123bc1bfc82c128d76ac3c07a9f
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-03-03 15:01:58 +0100

    prepath fix

--- tribble-additions/componentExtraction/FirefoxURLComponentsBuilder.java
+++ tribble-additions/componentExtraction/FirefoxURLComponentsBuilder.java
@@ -64,7 +64,8 @@ public class FirefoxURLComponentsBuilder extends URLComponentsBuilder {
         String prp="";
         prp+=escapeContent(univcomp.getComponentContents("scheme"));
         String input=escapeContent(univcomp.getComponentContents("input")); //TODO check what this does to relative URLs
-        if(input.startsWith(prp+"://")){
+        
+        if(input.startsWith(prp+"://")||spec.startsWith(prp+"://")){
             prp+="://";
         }
         else{
