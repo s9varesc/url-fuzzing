0745084744371fdd65b9f71ec0745f6ead5cbb92
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-03-25 14:29:53 +0100

    working on host unicode

--- grammars/livingstandard-url.scala
+++ grammars/livingstandard-url.scala
@@ -105,7 +105,7 @@ Grammar(
   'unicode := "[\u00a0-\ud7ff\ue000-\ufdcf\ufdf0-\ufffd]".regex , //this also contains rtl chars
              // | "[\u10000-\u1fffd]".regex, //TODO also use unicode above ffff
 
-  'hostunicode := ("[\u00a0-\ud7ff\ue000-\ufdcf\ufdf0-\ufffd-[\u0591-\u07ff\ufb1d-\ufdef\ufefc]]".regex ).rep(1,1), // avoid rtl unicode chars
+  'hostunicode := ("[\u00a0-\u0590\ue000-\ufb1e\ufdf0-\ufefb\ufefd-\ufffd]".regex ).rep(1,1), // avoid rtl unicode chars
   //("[\u0591-\u07ff\ufb1d-\ufdef\ufefc]".regex ~ ("[\u00a0-\ud7ff\ue000-\ufdcf\ufdf0-\ufffd]".regex | 'hostAllowed).rep ~"[\u0591-\u07ff\ufb1d-\ufdef\ufefc]".regex )
 
 
