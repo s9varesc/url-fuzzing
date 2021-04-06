1182bddf1b9155b9b3c4ea35227082317f3d7008
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-04-06 16:26:05 +0200

    encoding fix

--- evaluation-tools/browseroutputcleanup.py
+++ evaluation-tools/browseroutputcleanup.py
@@ -14,7 +14,7 @@ dir = args.dir
 
 for file in os.listdir(dir):
 	if file.endswith('.log'):
-		with open(dir +"/"+ file) as f:
+		with open(dir +"/"+ file, "r", encoding='utf-8') as f:
 			logfile=f.read()  
 			browsername=file[:-10] #remove "output.log"
 
@@ -33,10 +33,10 @@ for file in os.listdir(dir):
 					exceptionfiledata+=line+"\n" 
 	
 		#write resulting files
-		exfile=open(dir +"/"+ browsername+"Exceptions.txt", "w")
+		exfile=open(dir +"/"+ browsername+"Exceptions.txt", "w", encoding='utf-8')
 		exfile.write(exceptionfiledata)
 		exfile.close()
 
-		erfile=open(dir +"/"+ browsername+"Errors.txt", "w")
+		erfile=open(dir +"/"+ browsername+"Errors.txt", "w", encoding='utf-8')
 		erfile.write(errorfiledata)
 		erfile.close()	
