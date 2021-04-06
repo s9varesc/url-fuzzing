aa7623af0cc95bd17538615ffc1db0692f905505
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-04-06 12:41:05 +0200

    encoding fix for chromium style utility

--- chromium/stylefix.py
+++ chromium/stylefix.py
@@ -9,11 +9,11 @@ dir = args.dir
 
 for filename in os.listdir(dir):
     #print filename
-    f=open(dir+"/"+filename,  "r", encoding='utf-8')
-    s = f.read()
     if filename.endswith(".html"):
-        s = s.replace('../../../../../../../style.css', './style.css')
-    #print "replaced in " + filename
-    f=open(dir+"/"+filename, "w", encoding='utf-8')
-    f.write(s)
-    f.close()
+    	f=open(dir+"/"+filename,  "r", encoding='utf-8')
+    	s = f.read()
+    	s = s.replace('../../../../../../../style.css', './style.css')
+    	#print "replaced in " + filename
+    	f=open(dir+"/"+filename, "w", encoding='utf-8')
+    	f.write(s)
+    	f.close()
