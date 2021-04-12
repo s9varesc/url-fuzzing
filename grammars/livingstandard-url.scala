abb7122178de7478d1189998708a980fde92068a
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-04-12 13:02:08 +0200

    working on final grammar version

--- grammars/livingstandard-url.scala
+++ grammars/livingstandard-url.scala
@@ -2,7 +2,7 @@ import saarland.cispa.se.tribble.dsl._
 
 //following 4.3 URL writing at https://url.spec.whatwg.org/
 //including parts from https://tools.ietf.org/html/rfc3986#appendix-A and //https://tools.ietf.org/html/rfc6874
-//(whenever the living standard documentation was not sufficient to formulate a grammar)
+
 
 Grammar(
   'url := 'baseAndRelativeURL | 'absoluteURLwithFragment,
@@ -10,11 +10,11 @@ Grammar(
   'absoluteURL := 'specialAbsoluteURL | 'fileAbsoluteURL | 'otherAbsoluteURL,
 
   'URLspecialSchemeNotFile := "ftp" | "http" | "https" | "ws" | "wss", 
-  'URLnonSpecialScheme := 'alpha ~ ('alphanum | "+" | "-" | ".").rep,
+  'URLnonSpecialScheme := 'alpha ~ ('alphanum | "+" | "-" | ".").rep, //this includes many unregistered schemes, see https://www.iana.org/assignments/uri-schemes/uri-schemes.xhtml for a list of registered schemes
   'URLschemeFile := "file",
 
   'baseAndRelativeURL := ('specialBaseURL ~  "<" ~ 'specialRelativeURL )
-                          | ('fileBaseURL ~  "<" ~ 'fileRelativeURL )  //TODO might need to differentiate between empty/nonempty host
+                          | ('fileBaseURL ~  "<" ~ 'fileRelativeURL )  // differentiation between empty/nonempty host in base is not represented here
                           | ('otherBaseURL ~ "<" ~ 'otherRelativeURL ), 
 
 
@@ -31,22 +31,22 @@ Grammar(
   'fileRelativeURL := ('schemeRelativeFileURL | 'pathAbsoluteURL | 'pathAbsoluteNonWindowsFileURL | 'pathRelativeSchemelessURL) ~ ("?" ~ 'URLSpecialquery).? ~ ("#" ~ 'URLfragment).?,
   'otherRelativeURL := ('schemeRelativeURL | 'pathAbsoluteURL | 'pathRelativeSchemelessURL) ~ ("?" ~ 'URLquery).? ~ ("#" ~ 'URLfragment).?,
 
-  'schemeRelativeSpecialURL := "//" ~ ('domain | 'ipAddress) ~ ((":" ~ 'URLport.?).? ~ 'pathAbsoluteURL).?, 
+  'schemeRelativeSpecialURL := "//" ~ ('domain | 'ipAddress) ~ (":" ~ 'URLport.?).? ~ 'pathAbsoluteURL.?, 
   'schemeRelativeURL := "//" ~ 'opaqueHostAndPort ~ 'pathAbsoluteURL.?, 
   'schemeRelativeFileURL := "//" ~ ((('domain | 'ipAddress) ~ 'pathAbsoluteNonWindowsFileURL.?) | 'pathAbsoluteURL ),
   
-  'opaqueHostAndPort := 'opaqueHost ~ (":" ~ 'URLport).?, 
+  'opaqueHostAndPort := ('opaqueHost ~ (":" ~ 'URLport).?) | "", 
   'opaqueHost := ((('basicHost | 'opaqueHostPercentEncoded) ~ 'opaqueHostCodePoint.rep) | 'hostunicode) | ("[" ~ 'ipv6address ~ "]"), 
   'ipAddress:= 'ipv4address | ("[" ~ 'ipv6address ~ "]"),
   
   'pathAbsoluteURL := ("/"~'windowsDriveLetter.?).? ~ "/" ~ 'pathRelativeURLstart.?,
-  'pathAbsoluteNonWindowsFileURL := "/" ~ 'URLpathSegment ~ ("/" ~ 'pathRelativeURL).?,
-  'pathRelativeURL := 'URLpathSegment ~ ("/" ~ 'pathRelativeURL).? , //not allowed to start with /
-  'pathRelativeURLstart := (('firstPathCodePoint ~ 'pathCodePoint.rep) | 'singleDotPathSegment | 'doubleDotPathSegment) ~ ("/" ~ 'pathRelativeURL).?,  
-  'pathRelativeSchemelessURL := 'pathRelativeURLstart, // not allowed to start with scheme:
+  'pathAbsoluteNonWindowsFileURL := "/" ~ 'URLpathSegment ~ ("/" ~ 'pathRelativeURL).?, // not allowed to start with "/C:/" (windows drive letter)
+  'pathRelativeURL := 'URLpathSegment ~ ("/" ~ 'pathRelativeURL).? , //not allowed to start with /, use pathRelativeURLstart to force this
+  'pathRelativeURLstart := ((('firstPathCodePoint ~ 'pathCodePoint.rep) | 'singleDotPathSegment | 'doubleDotPathSegment) ~ ("/" ~ 'pathRelativeURL).?).?,  
+  'pathRelativeSchemelessURL := 'pathRelativeURLstart, // not allowed to start with "scheme:"
 
 
-  'windowsDriveLetter := 'alpha ~ (":" | "|"),
+  'windowsDriveLetter := 'alpha ~ ":", //only normalized drive letters are valid
   'URLpathSegment := ('pathCodePoint.rep) | 'singleDotPathSegment | 'doubleDotPathSegment,
   'singleDotPathSegment := "." | "%2e",
   'doubleDotPathSegment := ".." | ".%2e" | "%2e." | "%2e%2e", 
@@ -102,17 +102,18 @@ Grammar(
   'opaqueHostPercentEncoded := "%00" | "%09" | "%20" | "%23" | "%25" | "%2f" | "%3a" | "%3c" | "%3e" | "%3f" | "%40" | "%5b" | "%5c" | "%5d" | "%5e" | "%7c" ,
   
 
-  'unicode := "[\u00a0-\ud7ff\ue000-\ufdcf\ufdf0-\ufffd]".regex , //this also contains rtl chars
-             // | "[\u10000-\u1fffd]".regex, //TODO also use unicode above ffff
+  'unicode := "[\u00a0-\ud7ff\ue000-\ufdcf\ufdf0-\ufffd]".regex , //this excludes surrogates and noncharacters
+             // | "[\u10000-\u1fffd]".regex, // correctly enumerating unicode above \uffff for java 
 
   'hostunicode := "[\u0100-\u0148\u0148-\u017f]".regex,
     //"[\u024f-\u02af\u1050-\u1090\u10d0-\u10fa\u1200-\u1248\u1780-\u17b3\u1820-\u1877\ua000-\ua4fd\ua980-\ua9c0]".regex, 
   // this is a subset of code points allowed in hosts and far from exhaustive
+  // including more code points here requires: handling rtl and bidi characters, excluding unassigned code points, representation of code points above \uffff
 
 
   'queryCodePoint := 'specialQueryAllowed | "'" | 'queryPercentEncoded | 'unicode,
   'queryPercentEncoded := "%20" | "%22" | "%23" | "%3c" | "%3e",
-  'specialQueryAllowed := 'pathAllowed | "?" | "{" | "}" | "`" ,
+  'specialQueryAllowed := 'pathAllowed | "?" | "{" | "}" | "`" | "/",
   'specialQueryCodePoint := 'specialQueryAllowed |'queryPercentEncoded | 'unicode, //check if ' should be percent encoded
   
   //'userinfoCodePoint := 'userinfoAllowed | 'userinfoPercentEncoded | 'unicode,
@@ -120,13 +121,13 @@ Grammar(
   //'userinfoPercentEncoded := "%2f" | "%3a" | "%3b" | "%3d" | "%40" | "%5b" | "%5c" | "%5d" | "%5e" | "%7c",
 
   'pathCodePoint := 'pathAllowed | 'pathPercentEncoded | 'unicode,
-  'pathAllowed := 'userinfoAllowed | "/" | ":" | ";" | "=" | "@" | "[" | "]" |  "^" | "|",
+  'pathAllowed := 'userinfoAllowed | ":" | ";" | "=" | "@" | "[" | "]" |  "^" | "|",
   'pathPercentEncoded := 'queryPercentEncoded | "%3f" | "%60" | "%7b" | "%7d",
 
   'firstPathCodePoint := 'userinfoAllowed |  ";" | "=" | "@" | "[" | "]" |  "^" | "|" | 'pathPercentEncoded | 'unicode, //check if / and : should be percent encoded 
 
   'fragmentCodePoint := 'fragmentAllowed | 'fragmentPercentEncoded | 'unicode,
-  'fragmentAllowed := 'pathAllowed | "?" | "{" | "}" | "#",
+  'fragmentAllowed := 'pathAllowed | "?" | "{" | "}" | "#" | "/",
   'fragmentPercentEncoded := "%20" | "%22" | "%3c" | "%3e" | "%60",
  
 )
