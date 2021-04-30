5b6c1df28a4c21c5f0422ff1e7dbffe0cae12089
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-04-30 10:36:17 +0200

    adjust package structure to match current tribble version

--- tribble-additions/componentExtraction/DictExtractor.java
+++ tribble-additions/componentExtraction/DictExtractor.java
@@ -3,7 +3,7 @@ package de.cispa.se.tribble.componentExtraction;
 
   //import de.cispa.se.tribble._;
   //import DTree;
-  import de.cispa.se.tribble.generation.*;
+  import de.cispa.se.tribble.*;
   //import de.cispa.se.tribble.DerivationRule;
   //import de.cispa.se.tribble.Reference;
   
