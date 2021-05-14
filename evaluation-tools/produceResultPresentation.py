f276c61d76fe919f2170fb380c1750e25e6193a1
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-05-14 13:58:16 +0200

    added prettify cutoff for speed

--- evaluation-tools/produceResultPresentation.py
+++ evaluation-tools/produceResultPresentation.py
@@ -511,7 +511,9 @@ bov+= "*note:*  due to different component names the table above does not consid
 
 htmlresult1=markdown.markdown(bov, extensions=['extra'])
 htmlresult1=htmlresult1.replace("<table>", "<table class=\"simpletable\">")
-htmlresult2=markdown.markdown(bcomptable, extensions=['extra'])
+#htmlresult2=markdown.markdown(bcomptable, extensions=['extra'])
+htmlresult2="no table created"
+
 
 htmlhead="<!DOCTYPE html>\
 <html lang=\"en\">\
