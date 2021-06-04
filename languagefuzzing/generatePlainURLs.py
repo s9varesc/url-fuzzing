e625bc5498b93e0be553396ec2ca0a2f79b41fbb
Vera Resch <s9varesc@stud.uni-saarland.de>
2021-06-04 16:20:48 +0200

    bug fix

--- languagefuzzing/generatePlainURLs.py
+++ languagefuzzing/generatePlainURLs.py
@@ -25,7 +25,7 @@ f.close()
 
 for filename in os.listdir(dir+"/.."):
 	if "seed" in filename:
-		f=open(dir+"/../"+filename, "r", encoding='utf-8')
+		f=open(dir+"/../"+filename, "r")
 		seed=f.read()
 		f.close()
 		f=open("./../evaluation-tools/used_seed", "w")
